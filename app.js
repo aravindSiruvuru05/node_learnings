229edaef5e02b84791ae2031a5acadaa62566890
AravindSiruvuru-Sureify <aravind.siruvuru@sureify.com>
2021-03-27 23:29:46 +0530

    natours apis for all HTTP methods

--- app.js
+++ app.js
@@ -1,11 +1,111 @@
+// to import node inbulit packages like fs  with import statement we need to run in experimantal version
+// node --experimental-modules index.mjs     -- rename the file to .mjs extentin
+// import fs from "fs";
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ hi:'hellowsww'});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.use(express.json());
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid Id',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+  if (!tour)
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid Id',
+    });
+  const newTours = tours.map((t) => {
+    if (t.id === id) {
+      return { ...t, ...req.body };
+    }
+    return t;
+  });
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(newTours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour,
+        },
+      });
+    }
+  );
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((t) => t.id === id);
+  if (!tour)
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid Id',
+    });
+  const newTours = tours.filter((t) => t.id !== id);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(newTours),
+    (err) => {
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
