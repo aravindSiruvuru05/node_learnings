8129acd37386b5e7402cfb879808c195778fc014
AravindSiruvuru-Sureify <aravind.siruvuru@sureify.com>
2021-05-02 09:48:49 +0530

    refactor all quering methods to a class for reusablility

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,4 +1,5 @@
 const Tour = require('../modals/tourModal');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = 5;
@@ -9,50 +10,14 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // 1 FILITERING
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 2 ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 3 SORT
-
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 4 PROJECTING :: selectiong only some fileds needed
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 5 PAGENATION
-
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page doesnt exist');
-    }
-
-    // EXECUTE
-
-    const tours = await query;
+    const tours = await features.query;
 
     //SEND RESPONSE
     res.status(200).json({
