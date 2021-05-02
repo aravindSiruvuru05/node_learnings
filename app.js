34c52d787af8990dc78e911e7c0749c4fcf3006d
AravindSiruvuru-Sureify <aravind.siruvuru@sureify.com>
2021-05-02 13:48:28 +0530

    add ndb debugger tool and handle invalid route

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.use('/api/v1/tours', toursRoutes);
 
 app.use('/api/v1/users', usersRoutes);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on the server`,
+  });
+});
+
 module.exports = app;
 
 //NOTE: Routes
